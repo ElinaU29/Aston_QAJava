b1e5324a500dac590693fbe28dac171cb019581f
ElinaU29 <elinaurazaeva295@gmail.com>
2025-11-23 22:33:51 +0500

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Стандартный оператор вывода в консоль в Java
+        System.out.println("Привет, Мир! JDK установлен и работает.");
+    }
+}
