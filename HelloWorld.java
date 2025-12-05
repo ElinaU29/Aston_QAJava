822f76fffb7966e2ca7f2faa6cc736b76218bc25
ElinaU29 <elinaurazaeva295@gmail.com>
2025-12-05 21:17:54 +0500

    lesson_5 (убран файл HelloWorld)

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Стандартный оператор вывода в консоль в Java
-        System.out.println("Привет, Мир! JDK установлен и работает.");
-    }
-}
